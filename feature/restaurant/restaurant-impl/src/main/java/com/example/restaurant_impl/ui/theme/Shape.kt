ef768b4c35ef2c967523010bff6558420681d4f5
mashhood <mashhood.qazi@gudangada.com>
2022-11-25 02:10:04 +0500

    - Necessary modules created - Empty composable created from `Restaurant List` and `Sorting Bottom Sheet`

---
+++ feature/restaurant/restaurant-impl/src/main/java/com/example/restaurant_impl/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.restaurant_impl.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
